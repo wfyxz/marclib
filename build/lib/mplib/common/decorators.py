1277cdd9c590f82d40348fa77fc0ba46e0f48f4d
潘俊俊 <keyps2@icloud.com>
2016-12-14 18:51:17 +0800

    no message

--- build/lib/mplib/common/decorators.py
+++ build/lib/mplib/common/decorators.py
@@ -29,6 +29,6 @@ def cpu_elapse(function):
     def wrapper(*args, **kwargs):
         start = time.clock()
         ret = function(*args, **kwargs)
-        print "function '{0}' elapse {1} seconds".format(function.__name__, time.clock() - start)
+        print "function '{0}' elapse {1} single cpu core seconds".format(function.__name__, time.clock() - start)
         return ret
     return wrapper
