689885517ee2b5adbdfa3014c80fb6a829364192
潘俊俊 <keyps2@icloud.com>
2016-12-14 18:50:07 +0800

    调整时间统计的命名，新增程序总耗时的装饰器

--- build/lib/mplib/common/decorators.py
+++ build/lib/mplib/common/decorators.py
@@ -7,6 +7,25 @@ import time
 
 
 def time_elapse(function):
+    """
+    统计程序执行总时间消耗, 支持多进程的函数
+    :param function:
+    :return:
+    """
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        ret = function(*args, **kwargs)
+        print "function '{0}' elapse {1} seconds".format(function.__name__, time.time() - start)
+        return ret
+    return wrapper
+
+
+def cpu_elapse(function):
+    """
+    统计程序执行单个cpu core的时钟消耗, 不支持多进程函数
+    :param function:
+    :return:
+    """
     def wrapper(*args, **kwargs):
         start = time.clock()
         ret = function(*args, **kwargs)
